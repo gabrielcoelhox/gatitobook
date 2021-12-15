5209b51a4e5ff2556f44f3c83bf4aa8e48925db9
Gabriel Coelho <gabriel.coelhox@gmail.com>
2021-12-15 16:44:27 -0300

    Validacao cadastro novo usuario

---
+++ c
@@ -0,0 +1,23 @@
+[33mcommit 82b755a42b1522d433d0a563ab4a13f627f1ece4[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: Gabriel Coelho <gabriel.coelhox@gmail.com>
+Date:   Fri Dec 10 19:26:32 2021 -0300
+
+    Cadastro de Usuario
+
+[33mcommit e45e168cc5ae7eef45a339c02818e238e3ad2a26[m
+Author: Gabriel Coelho <gabriel.coelhox@gmail.com>
+Date:   Fri Dec 10 18:56:41 2021 -0300
+
+    Validação de campos e formulário
+
+[33mcommit 35e21cf79a4f6843a727592c74955c4dbd4cb2e7[m
+Author: Gabriel Coelho <gabriel.coelhox@gmail.com>
+Date:   Fri Dec 10 18:27:11 2021 -0300
+
+    Adicionando componente animais e autenticando login
+
+[33mcommit 0a4d24515eca2f64264add8b40c6b3fbb0d6185e[m
+Author: Gabriel Coelho <gabriel.coelhox@gmail.com>
+Date:   Fri Dec 10 17:13:58 2021 -0300
+
+    first commit
--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,7 +1,9 @@
+import { UsuarioExisteService } from './usuario-existe.service';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
+import { minusculoValidator } from './minusculo.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -13,14 +15,19 @@ export class NovoUsuarioComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private novoUsuarioService: NovoUsuarioService
+    private novoUsuarioService: NovoUsuarioService,
+    private usuarioExisteService: UsuarioExisteService
   ) {}
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      email: [''],
-      fullName: [''],
-      userName: [''],
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(4)]],
+      userName: [
+        '',
+        [minusculoValidator],
+        [this.usuarioExisteService.usuarioJaExiste()],
+      ],
       password: [''],
     });
   }
